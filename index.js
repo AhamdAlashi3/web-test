0abd98503c67dd2ad5d584d176c80fe1cb4455d5
Ahmad Alashi <72886433+AhamdAlashi3@users.noreply.github.com>
2023-01-29 12:25:52 +0200

    add user log

---
+++ index.js
@@ -0,0 +1,32 @@
+// const mydiv = document.getElementById('dd');
+// const but = document.querySelectorAll('button');
+
+
+// const arrayData = [
+//     '1',
+//     '2',
+//     '3',
+//     '4',    
+// ]
+
+// but.forEach((button,index) => {
+//     button.onclick = function(){
+//         mydiv.textContent =arrayData[index];
+//     };
+// });
+
+
+// const myButton = document.querySelector("button");
+
+// window.onscroll =()=>{
+//     if(window.scrollY >100){
+//         myButton.style.display='block';
+//     }else{
+//         myButton.style.display  = 'none';
+//     }
+// };
+
+// myButton.addEventListener('click',()=>{
+//     window.scrollTo(0,0);
+// })
+
