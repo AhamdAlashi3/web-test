90bad764018a984c93888ea162513441be3fab29
Ahmad Alashi <72886433+AhamdAlashi3@users.noreply.github.com>
2023-01-30 17:14:34 +0200

    add user log

--- index.js
+++ index.js
@@ -30,3 +30,48 @@
 //     window.scrollTo(0,0);
 // })
 
+
+// const myButton = document.querySelector("button");
+// const myDiv = document.querySelector("div");
+
+// let lang ='en';
+
+
+// const langs = {
+//     ar:{
+//         buttonText:'en',
+//         divText:"سلامات ي غالي"
+//     },
+//     en: {
+//         buttonText:'عربي',
+//         divText:"Hello ya ward"
+//     }
+// };
+
+// const toggelLang = ()=>{
+//     if(lang === 'en'){
+//         myButton.textContent=langs.ar.buttonText;
+//         myDiv.textContent=langs.ar.divText;
+//         lang='ar';
+//     }else{
+//         myButton.textContent=langs.en.buttonText;
+//         myDiv.textContent=langs.en.divText;
+//         lang='en';
+//     }
+//     window.localStorage.setItem("lang",lang);
+// }
+
+// myButton.onclick = toggelLang;
+
+
+const xhr= new XMLHttpRequest();
+
+xhr.onreadystatechange = ()=>{
+    if(xhr.readyState === 4){
+        console.log(xhr);
+    }
+};
+
+xhr.open("GET","https://jsonplaceholder.typicode.com/posts");
+xhr.send();
+
